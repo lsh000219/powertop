d3a4c3a05d3a529d2c3dbe5d79b2ee71c4c07ad5
Cloud User <centos@ip-172-31-1-157.ap-northeast-2.compute.internal>
2023-12-16 12:45:18 +0000

    m

--- src/instruction/instruction.h
+++ src/instruction/instruction.h
@@ -1,43 +1,30 @@
-//
-//
-// #ifndef INSTRUCTION_H
-// #define INSTRUCTION_H
-//
-// #include <vector>
-// #include <string>
-//
-// // 예시로 사용할 Instruction 데이터 구조
-// struct Instruction {
-//     std::string command;
-//         std::string description;
-//             // 다른 필요한 정보 추가 가능
-//             };
-//
-//             class instruction_window : public tab_window {
-//             public:
-//                 virtual void repaint(void);
-//                     virtual void cursor_enter(void);
-//                         virtual void expose(void);
-//                             virtual void window_refresh(void);
-//                             };
-//
-//                             // 초기화 함수
-//                             static void init_instruction(void);
-//
-//                             // Instruction 탭 초기화 함수
-//                             void initialize_instruction(void);
-//
-//                             // Instruction 탭 갱신 함수
-//                             void instruction_update_display(void);
-//
-//                             // Instruction 탭 초기화 함수
-//                             void clear_instruction(void);
-//
-//                             // Instruction 탭 화면 표시 함수
-//                             void show_instruction_tab();
-//
-//                             // Instruction 데이터 구조 벡터
-//                             extern std::vector<Instruction> instruction_all;
-//
-//                             #endif
-//
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include <vector>
+#include <string>
+
+struct Instruction {
+    std::string command;
+    std::string description;
+};
+
+class instruction_window : public tab_window {
+public:
+    virtual void repaint(void);
+    virtual void cursor_enter(void);
+    virtual void expose(void);
+    virtual void window_refresh(void);
+};
+
+static void init_instruction(void);
+
+void initialize_instruction(void);
+
+void instruction_update_display(void);
+
+void clear_instruction(void);
+
+extern std::vector<Instruction> instruction_all;
+
+#endif
