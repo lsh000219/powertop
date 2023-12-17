370daa2178db4ecb39a71334c41e1c47b6466d4b
Cloud User <centos@ip-172-31-40-201.ap-northeast-2.compute.internal>
2023-12-17 22:04:10 +0000

    r

--- src/main.cpp
+++ src/main.cpp
@@ -279,6 +279,7 @@ void one_measurement(int seconds, int sample_interval, char *workload)
 	show_report_devices();
 	report_show_open_devices();
 
+	initialize_instruction();
 	report_devices();
 	display_devfreq_devices();
 	report_devfreq_devices();
